939a8e7f93bc4f399bdb54290bb1f0b4722b3f63
Maciej Paszek <Maciej Paszek@DELL-G15-5515-M>
2025-09-26 15:35:07 +0200

    Linie Poziome

--- .NET MAUI/Biosensor pH/Controls/Chart.xaml.cs
+++ .NET MAUI/Biosensor pH/Controls/Chart.xaml.cs
@@ -20,6 +20,14 @@ public partial class Chart : ContentView, IDrawable
         set => SetValue(Chart.LineColorProperty, value);
     }
 
+    public static readonly BindableProperty UnitProperty = BindableProperty.Create(nameof(Unit), typeof(string), typeof(Chart), string.Empty);
+
+    public string Unit
+    {
+        get => (string)GetValue(Chart.UnitProperty);
+        set => SetValue(Chart.UnitProperty, value);
+    }
+
     public static readonly BindableProperty MinimumProperty = BindableProperty.Create(nameof(Minimum), typeof(double), typeof(Chart), 0.0);
 
     public double Minimum
@@ -34,14 +42,6 @@ public partial class Chart : ContentView, IDrawable
     {
         get => (double)GetValue(Chart.MaximumProperty);
         set => SetValue(Chart.MaximumProperty, value);
-    }
-
-    public static readonly BindableProperty UnitProperty = BindableProperty.Create(nameof(Unit), typeof(string), typeof(Chart), string.Empty);
-
-    public string Unit
-    {
-        get => (string)GetValue(Chart.UnitProperty);
-        set => SetValue(Chart.UnitProperty, value);
     }
 
     public static readonly BindableProperty MainScaleProperty = BindableProperty.Create(nameof(Maximum), typeof(double), typeof(Chart), 10.0);
@@ -60,6 +60,30 @@ public partial class Chart : ContentView, IDrawable
         set => SetValue(Chart.SecondaryScaleProperty, value);
     }
 
+    public static readonly BindableProperty TimeIntervalProperty = BindableProperty.Create(nameof(TimeInterval), typeof(TimeSpan), typeof(Chart), TimeSpan.FromMinutes(1));
+
+    public TimeSpan TimeInterval
+    {
+        get => (TimeSpan)GetValue(Chart.TimeIntervalProperty);
+        set => SetValue(Chart.TimeIntervalProperty, value);
+    }
+
+    public static readonly BindableProperty MainTimeScaleProperty = BindableProperty.Create(nameof(MainTimeScale), typeof(TimeSpan), typeof(Chart), TimeSpan.FromSeconds(10.0));
+
+    public TimeSpan MainTimeScale
+    {
+        get => (TimeSpan)GetValue(Chart.MainTimeScaleProperty);
+        set => SetValue(Chart.MainTimeScaleProperty, value);
+    }
+
+    public static readonly BindableProperty SecondaryTimeScaleProperty = BindableProperty.Create(nameof(SecondaryTimeScale), typeof(TimeSpan), typeof(Chart), TimeSpan.FromSeconds(1.0));
+
+    public TimeSpan SecondaryTimeScale
+    {
+        get => (TimeSpan)GetValue(Chart.SecondaryTimeScaleProperty);
+        set => SetValue(Chart.SecondaryTimeScaleProperty, value);
+    }
+
     public static readonly BindableProperty LineColorProperty = BindableProperty.Create(nameof(LineColor), typeof(Color), typeof(Chart), Colors.Gray);
 
     public Color LineColor
@@ -70,10 +94,22 @@ public partial class Chart : ContentView, IDrawable
 
     #endregion
 
+    private System.Timers.Timer _invalidateTimer;
+
     public Chart()
     {
         InitializeComponent();
         GraphicsView.Drawable = this;
+
+        _invalidateTimer = new System.Timers.Timer(1000);
+        _invalidateTimer.Elapsed += _invalidateTimer_Elapsed;
+        _invalidateTimer.AutoReset = true;
+        //_invalidateTimer.Start();
+    }
+
+    private void _invalidateTimer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+    {
+        GraphicsView.Invalidate();
     }
 
     #region IDrawable
@@ -81,6 +117,9 @@ public partial class Chart : ContentView, IDrawable
     private ICanvas? _canvas;
     private RectF _dirtyRect;
     private RectF _marginRect;
+
+    private DateTime _dateTimeNow;
+    private DateTime _dateTimeStart;
 
     public void Draw(ICanvas canvas, RectF dirtyRect)
     {
@@ -94,40 +133,10 @@ public partial class Chart : ContentView, IDrawable
 
         //DrawDebugRects();
 
-        double delta = Maximum - Minimum;
+        DrawHorizontalLines();
+        //DrawVerticalLines();
 
         canvas.StrokeColor = LineColor;
-
-        double value = Math.Ceiling(Minimum / SecondaryScale) * SecondaryScale;
-
-        while(value < Maximum)
-        {
-            // Nie rysuj tam, gdzie podzia³ka g³ówna
-            if (value % MainScale != 0)
-                DrawHorizontalLine(value);
-
-            value += SecondaryScale;
-        }
-
-        value = Math.Ceiling(Minimum / MainScale) * MainScale;
-
-        while (value < Maximum)
-        {
-            DrawHorizontalLine(value, $"{value} {Unit}");
-
-            value += MainScale;
-        }
-
-        DrawHorizontalLine(Minimum, $"{Minimum} {Unit}");
-        DrawHorizontalLine(Maximum, $"{Maximum} {Unit}");
-
-
-
-        for (int i = 0; i <= 10; i++)
-        {
-            DrawVerticalLine(i * 10);
-        }
-
     }
 
     private void DrawDebugRects()
@@ -148,14 +157,63 @@ public partial class Chart : ContentView, IDrawable
         return new RectF(marginLeft, marginTop, dirtyRect.Width - marginLeft - marginRight, dirtyRect.Height - marginTop - marginBottom);
     }
 
-    public float CalcX(double value)
+    public float CalcX(DateTime time)
     {
-        return (float)(_marginRect.Left + value * _marginRect.Width / 100);
+        return (float)(_marginRect.Left + (time - _dateTimeStart) * _marginRect.Width / TimeInterval);
     }
 
     public float CalcY(double value)
     {
         return (float) (_marginRect.Top + _marginRect.Height - (value - Minimum) * _marginRect.Height / (Maximum - Minimum));
+    }
+
+    public void DrawHorizontalLines()
+    {
+        double value = Math.Ceiling(Minimum / SecondaryScale) * SecondaryScale;
+
+        while (value < Maximum)
+        {
+            // Nie rysuj tam, gdzie podzia³ka g³ówna
+            if (value % MainScale != 0)
+                DrawHorizontalLine(value);
+
+            value += SecondaryScale;
+        }
+
+        value = Math.Ceiling(Minimum / MainScale) * MainScale;
+
+        while (value < Maximum)
+        {
+            DrawHorizontalLine(value, $"{value} {Unit}");
+
+            value += MainScale;
+        }
+
+        DrawHorizontalLine(Minimum, $"{Minimum} {Unit}");
+        DrawHorizontalLine(Maximum, $"{Maximum} {Unit}");
+    }
+
+    public void DrawVerticalLines()
+    {
+        _dateTimeNow = DateTime.Now;
+        _dateTimeStart = _dateTimeNow - TimeInterval;
+
+        DateTime time = _dateTimeStart;
+
+        while (time < _dateTimeNow)
+        {
+            DrawVerticalLine(time, time.ToString("HH:mm:ss"));
+            time += MainTimeScale;
+        }
+
+        time = _dateTimeStart;
+
+        while (time < _dateTimeNow)
+        {
+            DrawVerticalLine(time);
+            time += SecondaryTimeScale;
+        }
+
     }
 
     public void DrawHorizontalLine(double value)
@@ -182,16 +240,16 @@ public partial class Chart : ContentView, IDrawable
         _canvas.DrawString(text.ToString(), textRect, HorizontalAlignment.Right, VerticalAlignment.Center);
     }
 
-    public void DrawVerticalLine(float position)
+    public void DrawVerticalLine(DateTime time)
     {
-        float x = CalcX(position);
+        float x = CalcX(time);
         _canvas.StrokeColor = Colors.Gray;
         _canvas.DrawLine(x, _marginRect.Top, x, _marginRect.Bottom);
     }
 
-    public void DrawVerticalLine(float position, string text)
+    public void DrawVerticalLine(DateTime time, string text)
     {
-        float x = CalcX(position);
+        float x = CalcX(time);
 
         _canvas.StrokeColor = Colors.White;
         _canvas.DrawLine(x, _marginRect.Top, x, _marginRect.Bottom);
@@ -232,7 +290,7 @@ public partial class Chart : ContentView, IDrawable
 
         for (int i = 0; i < Data.Count; i++)
         {
-            float x = CalcX(Data[i].X);
+            float x = 0.0F;//CalcX(Data[i].X);
             float y = CalcY(Data[i].Y);
 
             if (i == 0)
